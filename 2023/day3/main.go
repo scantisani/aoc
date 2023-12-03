403b4f3e4dd3a6c240ce3bdd3cff7e46390e8fc0
Scott Cantisani <scantisani@gmail.com>
2023-12-03 19:07:40 +0100

    Day 3 Part 2 solution

--- 2023/day3/main.go
+++ 2023/day3/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"2023/day3/part1"
+	"2023/day3/part2"
 	"fmt"
 )
 
 func main() {
 	fmt.Println(part1.PartNumberSumFromInput())
+	fmt.Println(part2.GearRatioSumFromInput())
 }
