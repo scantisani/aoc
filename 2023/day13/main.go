8d2fe6dacec0f331da90553315cf4dafea0eee08
Scott Cantisani <scantisani@users.noreply.github.com>
2023-12-14 00:56:18 +0000

    Day 13 Part 2 solution

--- 2023/day13/main.go
+++ 2023/day13/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"2023/day13/part1"
+	"2023/day13/part2"
 	"fmt"
 )
 
 func main() {
 	fmt.Println(part1.SumOfReflectionsFromInput())
+	fmt.Println(part2.SumOfReflectionsFromInput())
 }
