f93e14faa82ed87ad1f8b10339e71336fac37f90
Scott Cantisani <scantisani@users.noreply.github.com>
2025-11-18 22:33:02 +0000

    2015 Day 4 Part 2 solution

---
+++ 2015/day4/part2/part2_test.go
@@ -0,0 +1,24 @@
+package part2
+
+import (
+	"testing"
+)
+
+type mineCoinTest struct {
+	input    string
+	expected int
+}
+
+var mineCoinTests = []mineCoinTest{
+	{"abcdef", 609043},
+	{"pqrstuv", 1048970},
+}
+
+func TestMineAdventCoin(t *testing.T) {
+	for _, test := range mineCoinTests {
+		received := MineAdventCoin(test.input)
+		if received != test.expected {
+			t.Errorf(`Should have been %d, was %d`, test.expected, received)
+		}
+	}
+}
